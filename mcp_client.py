4712e4d1f53fd734dcd8b10e7871c86e208b4b54
Zoltan C. Toth <zoltan@zoltanctoth.com>
2025-05-12 11:43:02 +0200

    beautify

--- mcp_client.py
+++ mcp_client.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 
-from mcp import ClientSession, StdioServerParameters, types
+from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
-
 
 ROOT_FOLDER = Path(__file__).parent.absolute()
 MCP_FOLDER = ROOT_FOLDER / "binance_mcp_reference_implementation"
 
 server_params = StdioServerParameters(
     command="python",  # Executable
-    args=[str(MCP_FOLDER / "binance_mcp.py")],  
-    env=None, 
+    args=[str(MCP_FOLDER / "binance_mcp.py")],
+    env=None,
 )
+
 
 async def run():
     async with stdio_client(server_params) as (read, write):
@@ -23,10 +23,9 @@ async def run():
                 "get_price", arguments={"symbol": "BTCUSDXXX"}
             )
             print(result)
-         
+
 
 if __name__ == "__main__":
     import asyncio
 
     asyncio.run(run())
-
